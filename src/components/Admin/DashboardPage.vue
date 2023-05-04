4c23ac8076a3a9465c4e95a8ae69a065bb524743
Paulo Pertierra <paulo.pertierra@gmail.com>
2023-05-04 09:23:19 +0800

    Chart integration succeed

--- src/components/Admin/DashboardPage.vue
+++ src/components/Admin/DashboardPage.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
+import AttendanceChart from "./DashboardPage/AttendanceChart.vue";
 import DashboardPage from "./DashboardPage/AttendancesCard.vue";
+import LineChart from "./DashboardPage/LineChart.vue";
 </script>
 <template>
   <div class="grid lg:grid-cols-4 md:grid-cols-1">
@@ -7,10 +9,10 @@ import DashboardPage from "./DashboardPage/AttendancesCard.vue";
       <DashboardPage />
     </div>
     <div class="col-span-1">
-      <DashboardPage />
+      <LineChart />
     </div>
-    <div class="col-span-1">
-      <DashboardPage />
+    <div class="col-span-1 p-4 mt-10">
+      <AttendanceChart />
     </div>
     <div class="lg:col-span-4 md:col-span-1">
       <DashboardPage />
---
+++ src/components/Admin/DashboardPage/LineChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-96 h-48 mt-10">
+    <p>Put this in a card so it looks cleaner</p>
+    <Pie :data="data" :options="options" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Pie } from 'vue-chartjs'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+const data = {
+  labels: ['On Leave', 'Present', 'AWOL'],
+  datasets: [
+    {
+      backgroundColor: ['#E46651', '#00D8FF', '#DD1B16'],
+      data: [20, 80, 10]
+    }
+  ]
+}
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false
+}
+
+</script>
