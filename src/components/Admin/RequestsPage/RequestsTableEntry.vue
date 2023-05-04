d64ce03271b2e1fbe28721315301026f593dfa7f
Paulo Pertierra <paulo.pertierra@gmail.com>
2023-05-04 08:53:15 +0800

    Requests form

--- src/components/Admin/RequestsPage/RequestsTableEntry.vue
+++ src/components/Admin/RequestsPage/RequestsTableEntry.vue
@@ -1,19 +1,31 @@
 <script lang="ts" setup>
 import moment from "moment";
 import axios from "axios";
+import Swal from "sweetalert2";
+import router from "@/router";
 moment.updateLocale(moment.locale(), { invalidDate: "No Date Specified" });
 const props = defineProps(["transaction"]);
 
 function approveRequest() {
-  axios
-    .put(`/transactions/${props.transaction.id}`)
-    .then((res)=>{
-      
-    })
+  axios.put(`/transactions/approve/${ props.transaction.id }`).then(
+    () => {
+      Swal.fire("Success.", "Request has been approved.", "success")
+      setInterval(()=>router.go(0), 1000)
+    }
+  ).catch(() => {
+    Swal.fire("Error", "Could not complete your request.", "error")
+  })
 }
 
 function declineRequest() {
-
+  axios.put(`/transactions/decline/${ props.transaction.id }`).then(
+    () => {
+      Swal.fire("Success.", "Request has been declined.", "success")
+      setInterval(()=>router.go(0), 1000)
+    }
+  ).catch(() => {
+    Swal.fire("Error", "Could not complete your request.", "error")
+  })
 }
 </script>
 <template>
@@ -39,9 +51,14 @@ function declineRequest() {
     <td class="px-6 py-4">
       {{ moment(props.transaction.endDate).format("LLL") }}
     </td>
-    <td class="px-6 py-4">{{ props.transaction.status }}</td>
-    <td class="px-6 py-4 text-right">
-      <button
+    <td class="px-6 py-4">
+      <span v-if="props.transaction.status === 'Approved'" class=" text-blue-500 font-bold">Approved</span>
+      <span v-if="props.transaction.status === 'Pending'" class=" text-orange-400 font-bold">Pending</span>
+      <span v-if="props.transaction.status === 'Declined'" class=" text-red-500 font-bold">Declined</span>
+    </td>
+    <td class="px-8 py-4 text-right">
+      <div v-if="props.transaction.status === 'Pending'">
+        <button
         @click="approveRequest()"
         class="mx-1 my-1 font-medium text-slate-600 dark:text-blue-500 w-12 border-blue-500 border rounded-lg hover:bg-blue-500 hover:text-white"
       >
@@ -54,6 +71,16 @@ function declineRequest() {
       >
         NO
       </button>
+      </div>
+      <div v-else>
+        <button
+        @click="pendingRequest()"
+        href="#"
+        class="mx-1 my-1 font-medium text-slate-600 dark:text-red-500 w-12 border-red-500 border rounded-lg hover:bg-red-500 hover:text-white"
+      >
+        RESET
+      </button>
+      </div>
     </td>
   </tr>
 </template>
